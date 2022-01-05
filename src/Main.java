b224b307eab97dbe7e65983ff9cc161b5b00857b
Marina Savenok <Savenok-marina@mail.ru>
2022-01-05 19:00:06 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int initially = 100;
+        int refill = 1850;
+
+        int bonus;
+        if (refill > 1000)  {
+            bonus = refill / 100;
+        }
+        else {
+            bonus = 0;
+        }
+
+        int balance = initially + refill + bonus;
+
+        System.out.println("Баланс: " + balance + " рублей. Бонус: " + bonus + " руб. ");
+
+        }
+}
